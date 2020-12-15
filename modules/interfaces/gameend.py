5dbf5ea25896b49f1cf998feedbd0d204d84fc4f
meowfy12 <75784796+meowfy12@users.noreply.github.com>
2020-12-16 01:41:09 +0800

    增加註解
    在gameend和gamestart的檔案，增加了簡單的註解，目的是讓大家能夠直接看懂每一行程式碼大概在做啥

--- modules/interfaces/gameend.py
+++ modules/interfaces/gameend.py
@@ -12,30 +12,31 @@ import pygame
 
 '''游戏结束界面'''
 def GameEndInterface(screen, cfg):
-	replay_image = pygame.image.load(cfg.IMAGE_PATHS['replay'])
-	replay_image = pygame.transform.scale(replay_image, (35, 31))
-	replay_image_rect = replay_image.get_rect()
+	replay_image = pygame.image.load(cfg.IMAGE_PATHS['replay'])  # 出現重新開始遊戲的圖片 載入圖片模組:圖片模組，用來處理載入圖片等相關操作，可當作角色精靈（sprite）
+	replay_image = pygame.transform.scale(replay_image, (35, 31))  # 縮放圖片 (寬，高) https://blog.csdn.net/wenxxxxx/article/details/9368213
+	replay_image_rect = replay_image.get_rect()  # 設定圖片顯示的位置
 	replay_image_rect.centerx = cfg.SCREENSIZE[0] / 2
 	replay_image_rect.top = cfg.SCREENSIZE[1] * 0.52
-	gameover_image = pygame.image.load(cfg.IMAGE_PATHS['gameover'])
-	gameover_image = pygame.transform.scale(gameover_image, (190, 11))
-	gameover_image_rect = gameover_image.get_rect()
+	gameover_image = pygame.image.load(cfg.IMAGE_PATHS['gameover'])  # 出現遊戲結束的圖片
+	gameover_image = pygame.transform.scale(gameover_image, (190, 11))  # 縮放圖片
+	gameover_image_rect = gameover_image.get_rect()  # 設定圖片顯示的位置
 	gameover_image_rect.centerx = cfg.SCREENSIZE[0] / 2
 	gameover_image_rect.centery = cfg.SCREENSIZE[1] * 0.35
-	clock = pygame.time.Clock()
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+	clock = pygame.time.Clock()  # 遊戲計時器(?)
+	# 事件迴圈監聽事件，進行事件處理
+    while True:
+		for event in pygame.event.get():  # 觸發事件  # 迭代整個事件迴圈，若有符合事件則對應處理
+			if event.type == pygame.QUIT:  # 當使用者結束視窗，程式也結束
 				pygame.quit()
 				sys.exit()
-			elif event.type == pygame.KEYDOWN:
+			elif event.type == pygame.KEYDOWN:  # KEYDOWN 按下鍵盤那瞬間
 				if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
 					return True
-			elif event.type == pygame.MOUSEBUTTONDOWN:
-				mouse_pos = pygame.mouse.get_pos()
-				if replay_image_rect.collidepoint(mouse_pos):
+			elif event.type == pygame.MOUSEBUTTONDOWN:  # MOUSEBUTTONDOWN 按下滑鼠那瞬間(?)
+				mouse_pos = pygame.mouse.get_pos() # get_pos()獲取滑鼠游標的位置
+				if replay_image_rect.collidepoint(mouse_pos):  # collidepoint 檢測滑鼠游標位置是不是在那個rec物件裡面
 					return True
-		screen.blit(replay_image, replay_image_rect)
-		screen.blit(gameover_image, gameover_image_rect)
-		pygame.display.update()
-		clock.tick(cfg.FPS)
+		screen.blit(replay_image, replay_image_rect)  # blit 把某東西建置到螢幕上面  # 建置重新開始的圖片和rect
+		screen.blit(gameover_image, gameover_image_rect)  # 建置遊戲結束的圖片和rect
+		pygame.display.update()  # 更新繪圖視窗內容，以顯示繪製的圖形
+		clock.tick(cfg.FPS)  # 更新時鐘
--- modules/interfaces/gamestart.py
+++ modules/interfaces/gamestart.py
@@ -8,31 +8,31 @@ Author:
 '''
 import sys
 import pygame
-from modules.sprites.dinosaur import Dinosaur
+from modules.sprites.dinosaur import Dinosaur  # 匯入恐龍
 
 
 '''游戏开始界面'''
-def GameStartInterface(screen, sounds, cfg):
-	dino = Dinosaur(cfg.IMAGE_PATHS['dino'])
-	ground = pygame.image.load(cfg.IMAGE_PATHS['ground']).subsurface((0, 0), (83, 19))
-	rect = ground.get_rect()
-	rect.left, rect.bottom = cfg.SCREENSIZE[0]/20, cfg.SCREENSIZE[1]
-	clock = pygame.time.Clock()
-	press_flag = False
+def GameStartInterface(screen, sounds, cfg):  # 匯入屏幕、聲音 cfg(?)
+	dino = Dinosaur(cfg.IMAGE_PATHS['dino'])  # 匯入恐龍圖片
+	ground = pygame.image.load(cfg.IMAGE_PATHS['ground']).subsurface((0, 0), (83, 19))  # 匯入大地圖片
+	rect = ground.get_rect()  # 設定大地圖片的位置
+	rect.left, rect.bottom = cfg.SCREENSIZE[0]/20, cfg.SCREENSIZE[1]  # 把大地圖片設定在左下角(?)(後面的還沒看懂)
+	clock = pygame.time.Clock()  # 設定遊戲計時器
+	press_flag = False  # 先設定開始按鈕為F(?)
 	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
+		for event in pygame.event.get():  # 事件
+			if event.type == pygame.QUIT:  # 離開遊戲
 				pygame.quit()
 				sys.exit()
 			elif event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
 					press_flag = True
-					dino.jump(sounds)
-		dino.update()
-		screen.fill(cfg.BACKGROUND_COLOR)
-		screen.blit(ground, rect)
-		dino.draw(screen)
-		pygame.display.update()
-		clock.tick(cfg.FPS)
+					dino.jump(sounds)  # 恐龍跳起來
+		dino.update()  # 恐龍狀態更新
+		screen.fill(cfg.BACKGROUND_COLOR)  # 將螢幕填充背景顏色
+		screen.blit(ground, rect)  #將大地圖片和大地圖片的位置放到(建構)螢幕上
+		dino.draw(screen)  # 把恐龍畫到螢幕上
+		pygame.display.update()  # 遊戲狀態更新
+		clock.tick(cfg.FPS)  # 開始計時
 		if (not dino.is_jumping) and press_flag:
 			return True
